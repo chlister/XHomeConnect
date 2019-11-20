1b83599cbb78cc63de844566e46d6491509bb300
chlister <mr-jmp@hotmail.com>
2019-11-20 12:51:13 +0100

    Interface added

---
+++ src/main/java/IApiClientCallback.java
@@ -0,0 +1,32 @@
+import websocket.SocketDTO;
+import java.util.List;
+
+/**
+ * TODO: Add interface description
+ *
+ * @author Marc R. K.
+ * @version 1.0
+ * @since 11/20/19
+ */
+
+public interface IApiClientCallback {
+
+    /**
+     * Used to return a list of socketDTO objects
+     * @return List<SocketDTO>
+     */
+    List<SocketDTO>getSockets();
+
+    /**
+     * Used to search local net for units matching the Netio agent signature.
+     */
+    void detectLocalAgents();
+
+    /**
+     * Used to register a specific socket.
+     * @param socket Socket object containing the info to be updated.
+     * @return SocketDTO same object as send to denote success from the receivers end.
+     */
+    SocketDTO registerSocket(SocketDTO socket);
+}
+
