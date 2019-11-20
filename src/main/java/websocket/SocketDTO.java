debfe2258036506278c7e3b9f8094382908db063
chlister <mr-jmp@hotmail.com>
2019-11-20 12:55:03 +0100

    websocket package added Socket DTO added

---
+++ src/main/java/websocket/SocketDTO.java
@@ -0,0 +1,15 @@
+/**
+ * @author Marc R. K.
+ * @version 1.0
+ * @since 11/20/19
+ */
+
+package websocket;
+
+/**
+ * Socket DTO to be sent over HTTP connection
+ */
+public class SocketDTO {
+    int id, agentId;
+    String name, applianceType;
+}
