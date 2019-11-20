7fecefba22d833d9a6b60cba5c6fbde2216fda7b
chlister <mr-jmp@hotmail.com>
2019-11-20 12:50:46 +0100

    XHomeConnect class added

---
+++ src/main/java/HomeController.java
@@ -0,0 +1,28 @@
+/**
+ * @author Marc R. K.
+ * @version 1.0
+ * @since 11/20/19
+ */
+
+/**
+ * Main class of the XHomeConnect module.
+ */
+public class HomeController {
+//    IAgentManager agentManager - TODO class not defined
+//    IWebSocketManager webSocketManager - TODO class not defined
+//    IApiClientManager apiClientManager - TODO class not defined
+
+    /**
+     * Initialises the XHomeConnect module.
+     * @author  Marc R. K.
+     * @version 1.0
+     * @since   11/20/19
+     * @status  Defined
+     */
+    void Init() {
+
+    }
+}
+
+
+
