6a84d0a22f1a2410fad4486222e77725d3469c30
chlister <mr-jmp@hotmail.com>
2019-11-20 13:26:04 +0100

    Homecontroller test class made ready - Test can be written when the other classes are ready

---
+++ src/test/java/HomeControllerTest.java
@@ -0,0 +1,14 @@
+import org.junit.Test;
+
+/**
+ * @author Marc R. K.
+ * @version 1.0
+ * @since 11/20/19
+ */
+
+public class HomeControllerTest {
+    @Test
+    void initTest(){
+        // TODO test to be defined when other packages are ready
+    }
+}
