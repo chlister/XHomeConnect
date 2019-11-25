72646beb32500de2fbfa77c0e069053464f6b358
chlister <mr-jmp@hotmail.com>
2019-11-25 13:28:58 +0100

    Removed comments because they don't make sense until implementation

--- src/main/java/com/xpower/xhomeconnect/websocket/IWebSocketManager.java
+++ src/main/java/com/xpower/xhomeconnect/websocket/IWebSocketManager.java
@@ -8,24 +8,8 @@ package com.xpower.xhomeconnect.websocket;
 
 public interface IWebSocketManager {
 
-    /**
-     * listener event when com.xpower.xhomeconnect.websocket opens
-     */
     void onOpen();
-
-    /**
-     * listener event when the connection closes (failure or otherwise)
-     */
     void onClose();
-
-    /**
-     * When the client sends a message to the com.xpower.xhomeconnect.websocket
-     */
     void onMessage();
-
-    /**
-     * When a socket needs to be updated
-     * @param socket SocketDTO
-     */
     void updateSocket(SocketDTO socketDTO);
 }
