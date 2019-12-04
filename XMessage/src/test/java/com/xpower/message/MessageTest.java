8510c9d0022480849fabc59a73e8794586c79541
chlister <mr-jmp@hotmail.com>
2019-12-04 18:45:04 +0100

    Test on XMessage uploaded

---
+++ XMessage/src/test/java/com/xpower/message/MessageTest.java
@@ -0,0 +1,32 @@
+package com.xpower.message;
+
+import com.xpower.message.model.OutletDTO;
+import org.junit.Assert;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+public class MessageTest {
+    String testJson = "{\"mRespondCodes\":\"OK\",\"mMethodCode\":\"GET_SOCKETS\",\"mObj\":{\"id\":1,\"agentId\":1,\"state\":false,\"name\":\"Test\",\"applianceType\":\"NON\"}}";
+
+    Message testObject = new Message(
+            RespondCodes.OK,
+            MethodCode.GET_SOCKETS,
+            new OutletDTO(1,1,"Test", "NON", false)
+    );
+
+
+    @org.junit.Test
+    public void Message() {
+        Assert.assertEquals(testObject.getMethodCode(), new Message(testJson).getMethodCode());
+        Assert.assertEquals(testObject.getRespondCodes(), new Message(testJson).getRespondCodes());
+    }
+
+    @org.junit.Test
+    public void encode() {
+        Assert.assertEquals(testJson, testObject.encode());
+
+    }
+
+
+}
